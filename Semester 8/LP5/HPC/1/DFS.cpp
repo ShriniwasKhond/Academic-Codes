3102a647b638354fe50dd2c0daf5f677899d7c2b
Shriniwas khond <skhond32@gmail.com>
2023-05-28 12:50:24 +0530

    Update DFS.cpp

--- Semester 8/LP5/HPC/1/DFS.cpp
+++ Semester 8/LP5/HPC/1/DFS.cpp
@@ -1,42 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <omp.h>
 
 using namespace std;
 
-void dfs(vector<vector<int>>& adj, vector<bool>& visited, int node) {
+void dfs(vector<vector<int>>& graph, vector<bool>& visited, int node) {
     visited[node] = true;
-    cout << node << " ";
+    cout << node << " "; 
 
-    for (int next_node : adj[node]) {
+    #pragma omp parallel for
+    for (int next_node : graph[node]) {
         if (!visited[next_node]) {
-            dfs(adj, visited, next_node);
+            dfs(graph, visited, next_node);
         }
     }
 }
 
 int main() {
-    int n, m;
+    int n, m; 
     cout << "Enter the number of nodes and edges: ";
     cin >> n >> m;
 
-    vector<vector<int>> adj(n); 
-    vector<bool> visited(n, false); 
+    vector<vector<int>> graph(n + 1); 
+    vector<bool> visited(n + 1, false); 
 
     cout << "Enter the edges: " << endl;
     for (int i = 1; i <= m; i++) {
         int u, v; 
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
 
-    int start_node;
+    int start_node; // start node of DFS
     cout << "Enter the start node: ";
     cin >> start_node;
 
+    double start_time = omp_get_wtime();
+
     cout << "DFS traversal starting from node " << start_node << ": ";
-    dfs(adj, visited, start_node);
+    dfs(graph, visited, start_node);
     cout << endl;
+
+    double end_time = omp_get_wtime();
+    double execution_time = end_time - start_time;
+
+    cout << "Execution time: " << execution_time << " seconds" << endl;
 
     return 0;
 }
